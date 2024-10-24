5d33b139e4a49b1b8069f3deefd2b20f3b592b13
zaffaelma <zaffazidnielma@gmail.com>
2024-10-24 13:18:45 +0700

    mengubah return

--- app.js
+++ app.js
@@ -10,17 +10,17 @@
 
 // TULIS SCRIPT KAMU DI SINI
 function calculateKomisi(nilaiKontrak) {
-  if (nilaiKontrak > 1000000) {
-    return result= 100000;
-  } else if (nilaiKontrak > 5000000) {
-    return result = nilaiKontrak * 0.05;
+  if (nilaiKontrak >= 5000000) {
+    return nilaiKontrak * 0.05;
+  } else if (nilaiKontrak >= 1000000) {
+    return 100000;
   } else {
-    return result = 0
+    return 0
   }
 }
-nilaiKontrak = (100000)
+nilaiKontrak = (5000000)
 result = calculateKomisi(nilaiKontrak)
-console.log("Jika nilai kontrak adalah 100.000, maka nilai komisi adalah " + result + ".")
+console.log("Jika nilai kontrak adalah " + nilaiKontrak + " maka nilai komisi adalah " + result + ".")
 
 // JANGAN HAPUS / UBAH SCRIPT DI BAWAH
 module.exports = calculateKomisi
